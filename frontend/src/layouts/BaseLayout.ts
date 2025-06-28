e2fa9280af67261c406cd7169e0edabbbb398b29
odudniak <odudniak@student.42firenze.it>
2025-06-28 18:34:01 +0200

    feat: enhance user menu with settings and logout options for logged-in users

--- frontend/src/layouts/BaseLayout.ts
+++ frontend/src/layouts/BaseLayout.ts
@@ -8,6 +8,27 @@ export class BaseLayout extends LayoutController {
 	async render() {
 		const isLoggedIn = await authManager.isUserLoggedIn();
 
+		let userMenuButtons: string = '';
+		if (isLoggedIn && authManager.user) {
+			userMenuButtons =  /*html*/`
+				<div class="hover:text-emerald-400 cursor-pointer">
+					<i class="fa fa-cog"></i>
+					Settings
+				</div>
+				<a href="/api/auth/logout" class="hover:text-emerald-400">
+					<i class="fa fa-sign-out"></i>
+					Logout
+				</a>
+			`;
+		} else {
+			userMenuButtons =  /*html*/`
+			<a href="/api/auth/login/google" class="hover:text-emerald-400">
+				<i class="fa fa-sign-in"></i>
+				Login
+			</a>
+			`;
+		}
+
 		return /*html*/`
 			<div class="flex flex-col grow w-full bg-neutral-900 text-white">
 				<header class="flex items-center h-20 px-4 shadow-xl bg-neutral-950">
@@ -15,33 +36,28 @@ export class BaseLayout extends LayoutController {
 					<div class="text-xl relative">
 						<div id="${AUTH_DOM_IDS.userMenuButton}" class="flex items-center p-2 cursor-pointer ">
 							${isLoggedIn && authManager.user
-			? /*html*/` <img src="${authManager.userImageUrl}" alt="Logged in user image" class="rounded-full w-10 h-10">`
-			: /*html*/`<i class="fa fa-2x fa-user-circle hover:text-emerald-100"></i>`
-		}
+								? /*html*/`<img src="${authManager.userImageUrl}" alt="Logged in user image" class="rounded-full w-10 h-10">`
+								: /*html*/`<i class="fa fa-2x fa-user-circle hover:text-emerald-100"></i>`
+							}
 						</div>
 
 						<div id="${AUTH_DOM_IDS.userMenuContainer}" class="absolute top-full right-0 hidden w-40 bg-black rounded px-3 py-1 text-base items-center">
-							<div class="flex flex-col gap-1 w-full">
-								<h3 class="text-center">${authManager.user?.username}</h3>
-								<hr class="text-white/10"/>
+							<div class="flex flex-col gap-1 w-full select-none">
+								${isLoggedIn
+									? /*html*/`
+										<h3 class="text-center">${authManager.user?.username}</h3>
+										<hr class="text-white/10"/>
+									`
+									: ''
+								}
 
-							${isLoggedIn && authManager.user ? /*html*/`
-								<div class="hover:text-emerald-400">
-									<i class="fa fa-cog"></i>
-									Settings
-								</div>
-								<a href="/api/auth/logout" class="hover:text-red-200">
-									<i class="fa fa-sign-out"></i>
-									Logout
-								</a>` : ''
-		}
+								${userMenuButtons}
 							</div>
 						</div>
 					</div>
 				</header>
 				<div id="app_layout_content" class="flex grow flex-col w-full"></div>
-			</div>
-		`;
+			</div>`;
 	}
 
 	async postRender() {
