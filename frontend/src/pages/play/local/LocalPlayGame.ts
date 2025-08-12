bfdfacdb711fce4ee2841745e185f3b791c23d78
Sandoramix <sandoramix.dev@gmail.com>
2025-08-12 03:16:20 +0200

    feat: added LocalPlayGame route for local gameplay functionality in the frontend

---
+++ frontend/src/pages/play/local/LocalPlayGame.ts
@@ -0,0 +1,11 @@
+import { RouteController } from '@tools/ViewController';
+export class LocalPlayGame extends RouteController {
+
+
+	async render(){
+		return /*html*/`
+			<p>Local Play Game works</p>
+		`;
+	}
+
+}
